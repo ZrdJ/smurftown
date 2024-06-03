540e4f7523ad2ab5c2ceddc9ec3a9df92c8daa21
Aleksandar Damjanovic <codejanovic@zrdj.org>
2024-06-03 10:23:56 +0200

    fixed account card image source

--- Smurftown/UI/MVVM/ViewModel/AccountCardViewModel.cs
+++ Smurftown/UI/MVVM/ViewModel/AccountCardViewModel.cs
@@ -17,8 +17,21 @@ namespace Smurftown.UI.MVVM.ViewModel
         public BattlenetAccount? Account
         {
             get { return _account; }
-            set { _account = value; OnPropertyChanged(); }
+            set { 
+                _account = value;
+                ImageSource = value.Overwatch ? "pack://application:,,,/UI/Images/overwatch_full.png" : "pack://application:,,,/UI/Images/hots_full.png";
+                OnPropertyChanged(); 
+            }
         }
+
+        private string _imageSource;
+
+        public string ImageSource
+        {
+            get { return _imageSource; }
+            set { _imageSource = value; OnPropertyChanged(); }
+        }
+
 
         public AccountCardViewModel(BattlenetAccount account) => Account = account;
 
