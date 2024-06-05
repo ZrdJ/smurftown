16154b73a853ab2fc5576e50bbd477f36e472106
Aleksandar Damjanovic <codejanovic@zrdj.org>
2024-06-05 14:10:17 +0200

    added open battlenet functionality

--- Smurftown/Backend/Gateway/WindowsAccountGateway.cs
+++ Smurftown/Backend/Gateway/WindowsAccountGateway.cs
@@ -1,40 +1,32 @@
-﻿using System.Management;
+﻿using System.Diagnostics;
+using System.Management;
 using System.Runtime.InteropServices;
-using System.Windows.Documents;
 using Smurftown.Backend.Entity;
 
 namespace Smurftown.Backend.Gateway
 {
     public class WindowsAccountGateway
     {
-        public static readonly WindowsAccountGateway Instance = new();
-        
-        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
-        struct USER_INFO_1
-        {
-            public string usri1_name;
-            public string usri1_password;
-            public uint usri1_password_age;
-            public uint usri1_priv;
-            public string usri1_home_dir;
-            public string usri1_comment;
-            public uint usri1_flags;
-            public string usri1_script_path;
-        }
-
-        [DllImport("Netapi32.dll", CharSet = CharSet.Unicode)]
-        private static extern int NetUserAdd([MarshalAs(UnmanagedType.LPWStr)] string servername, uint level,ref USER_INFO_1 buf,out uint parm_err);
-
         private const uint USER_PRIV_USER = 1;
         private const uint UF_SCRIPT = 1;
         private const uint UF_DONT_EXPIRE_PASSWD = 0x10000;
+        public static readonly WindowsAccountGateway Instance = new();
 
         private List<WindowsUserAccount> _windowsAccounts;
-        public IReadOnlyList<WindowsUserAccount> WindowsAccounts { get => _windowsAccounts.AsReadOnly(); }
+
         private WindowsAccountGateway()
         {
             _windowsAccounts = [..ReadWindowsAccounts()];
         }
+
+        public IReadOnlyList<WindowsUserAccount> WindowsAccounts
+        {
+            get => _windowsAccounts.AsReadOnly();
+        }
+
+        [DllImport("Netapi32.dll", CharSet = CharSet.Unicode)]
+        private static extern int NetUserAdd([MarshalAs(UnmanagedType.LPWStr)] string servername, uint level,
+            ref USER_INFO_1 buf, out uint parm_err);
 
         public void Add(WindowsUserAccount account)
         {
@@ -45,13 +37,13 @@ namespace Smurftown.Backend.Gateway
         {
             var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_UserAccount");
             return (from ManagementObject user in searcher.Get()
-                    select new WindowsUserAccount
-                    {
-                        Name = user["Name"].ToString(),
-                        Password = user["Name"].ToString().ToLower()
-                    }).ToList();
-           
+                select new WindowsUserAccount
+                {
+                    Name = user["Name"].ToString(),
+                    Password = user["Name"].ToString().ToLower()
+                }).ToList();
         }
+
         private static void CreateWindowsAccount(WindowsUserAccount account)
         {
             var userInfo = new USER_INFO_1
@@ -73,6 +65,74 @@ namespace Smurftown.Backend.Gateway
         public void Reload()
         {
             _windowsAccounts = [..ReadWindowsAccounts()];
+        }
+
+        private string ToWindowsUser(BattlenetAccount b)
+        {
+            return (b.Name + "-" + b.Discriminator).ToLower();
+        }
+
+        public void OpenBattlenet(BattlenetAccount account)
+        {
+            const string programPath = @"C:\Program Files (x86)\Battle.net\Battle.net.exe";
+            var username = ToWindowsUser(account);
+            var windowsUser = _windowsAccounts.Find(u => u.Name.Equals(username));
+            const string domain = ".";
+
+            // Create a new process start info
+            var startInfo = windowsUser == null
+                ? new ProcessStartInfo
+                {
+                    FileName = programPath,
+                    UseShellExecute = true, // Must be false to use UserName and Password
+                    LoadUserProfile = false // Optionally load the user profile
+                }
+                : new ProcessStartInfo
+                {
+                    FileName = programPath,
+                    UserName = username,
+                    Password = ConvertToSecureString(username),
+                    Domain = domain,
+                    UseShellExecute = false, // Must be false to use UserName and Password
+                    LoadUserProfile = false // Optionally load the user profile
+                };
+
+            try
+            {
+                // Start the process with the specified user credentials
+                using var process = Process.Start(startInfo);
+                Console.WriteLine("Process started successfully.");
+                //process?.WaitForExit(); // Optionally wait for the process to exit
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private static System.Security.SecureString ConvertToSecureString(string password)
+        {
+            if (password == null)
+                throw new ArgumentNullException(nameof(password));
+
+            var securePassword = new System.Security.SecureString();
+            foreach (char c in password)
+                securePassword.AppendChar(c);
+            securePassword.MakeReadOnly();
+            return securePassword;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        struct USER_INFO_1
+        {
+            public string usri1_name;
+            public string usri1_password;
+            public uint usri1_password_age;
+            public uint usri1_priv;
+            public string usri1_home_dir;
+            public string usri1_comment;
+            public uint usri1_flags;
+            public string usri1_script_path;
         }
     }
 }
--- Smurftown/UI/MVVM/ViewModel/AccountCardViewModel.cs
+++ Smurftown/UI/MVVM/ViewModel/AccountCardViewModel.cs
@@ -1,40 +1,79 @@
-﻿using Smurftown.Backend.Entity;
+﻿using System.Windows.Input;
+using Smurftown.Backend.Entity;
+using Smurftown.Backend.Gateway;
 
 namespace Smurftown.UI.MVVM.ViewModel
 {
-    class AccountCardViewModel: Observable
+    class AccountCardViewModel : Observable
     {
-
+        private static readonly WindowsAccountGateway _windowsAccountGateway = WindowsAccountGateway.Instance;
         private BattlenetAccount? _account;
+
+        private string _imageSource;
+
+
+        private RelayCommand _openBattlenetCommand;
+        public AccountCardViewModel(BattlenetAccount account) => Account = account;
+
+        public AccountCardViewModel()
+        {
+        }
 
         public BattlenetAccount? Account
         {
             get { return _account; }
-            set { 
+            set
+            {
                 _account = value;
                 if (value.Overwatch && value.Hots)
                 {
-                    ImageSource =  "pack://application:,,,/UI/Images/overwatchhots_full.png";
-                } else
-                {
-                    ImageSource = value.Overwatch ? "pack://application:,,,/UI/Images/overwatch_full.png" : "pack://application:,,,/UI/Images/hots_full.png";
+                    ImageSource = "pack://application:,,,/UI/Images/overwatchhots_full.png";
                 }
-                
-                OnPropertyChanged(); 
+                else
+                {
+                    ImageSource = value.Overwatch
+                        ? "pack://application:,,,/UI/Images/overwatch_full.png"
+                        : "pack://application:,,,/UI/Images/hots_full.png";
+                }
+
+                OnPropertyChanged();
             }
         }
-
-        private string _imageSource;
 
         public string ImageSource
         {
             get { return _imageSource; }
-            set { _imageSource = value; OnPropertyChanged(); }
+            set
+            {
+                _imageSource = value;
+                OnPropertyChanged();
+            }
         }
 
+        public ICommand OpenBattlenetCommand
+        {
+            get
+            {
+                if (_openBattlenetCommand == null)
+                {
+                    _openBattlenetCommand = new RelayCommand(
+                        param => this.OpenBattlenet(),
+                        param => this.CanOpenBattlenet()
+                    );
+                }
 
-        public AccountCardViewModel(BattlenetAccount account) => Account = account;
+                return _openBattlenetCommand;
+            }
+        }
 
-        public AccountCardViewModel() { }
+        private bool CanOpenBattlenet()
+        {
+            return true;
+        }
+
+        private void OpenBattlenet()
+        {
+            _windowsAccountGateway.OpenBattlenet(_account);
+        }
     }
 }
